7e8e1ec757d48392b4977bb1f98dc91a1f5eaf80
Cristian Rivera <89621309+crezdb1425@users.noreply.github.com>
2023-11-27 18:43:24 -0300

    RSTP protocol para streaming de video

---
+++ ESP32-RTSP-master/src/wifikeys.h
@@ -0,0 +1,4 @@
+#include <Arduino.h>
+// Change YOUR_AP_NAME and YOUR_AP_PASSWORD to your WiFi credentials
+const char *ssid = "NITRO5 9222";		   // Put your SSID here
+const char *password = "m07962[N"; // Put your PASSWORD here
