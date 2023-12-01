733eeadbed0987afb6e4de8d5d66f455059023ee
Cristian Rivera <89621309+crezdb1425@users.noreply.github.com>
2023-12-01 20:03:39 -0300

    Delete ESP32-RTSP-master directory
    No se utiliza rtsp

--- ESP32-RTSP-master/src/wifikeys.h
+++
@@ -1,4 +0,0 @@
-#include <Arduino.h>
-// Change YOUR_AP_NAME and YOUR_AP_PASSWORD to your WiFi credentials
-const char *ssid = "NITRO5 9222";		   // Put your SSID here
-const char *password = "m07962[N"; // Put your PASSWORD here
