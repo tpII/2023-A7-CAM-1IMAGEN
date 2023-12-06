3d8e292be8386e9bb6ed5b642e1dabe4340aca7a
Cristian Rivera <89621309+crezdb1425@users.noreply.github.com>
2023-12-06 17:05:55 -0300

    Add files via upload

---
+++ mqtt.py
@@ -0,0 +1,10 @@
+import paho.mqtt.publish as publish
+
+mqtt_server = "broker.mqtt-dashboard.com"
+mqtt_port = 1883
+topic = "inTopic"
+
+def send_message(message1, message2):
+    publish.single(topic, f"{message1},{message2}", hostname=mqtt_server, port=mqtt_port)
+
+send_message(20, 241)
